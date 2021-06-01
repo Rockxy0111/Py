746ca0186441a448ae90fdc3210bc7cce810c7df
RanveerKhokhar <72133434+Rockxy0111@users.noreply.github.com>
2021-06-01 18:16:27 +0530

    Update Swappingfile.py

--- Swappingfile.py
+++ Swappingfile.py
@@ -1,17 +1,18 @@
 def SwapFileData ():
 
     file1=input("Enter your file name:")
-    with open(file1) as a:
+    file2=input("Enter your file name:")
+    with open(file1,'r') as a:
         data_a=a.read()
 
-    with open(file1) as a:
+    with open(file2,'r') as a:
         data_b=a.read()
 
-    with open(file1) as a:
+    with open(file1,'w') as a:
         a.write(data_b)
 
 
-    with open(file1) as a:
+    with open(file2,'w') as a:
         a.write(data_a)
 
 SwapFileData()
